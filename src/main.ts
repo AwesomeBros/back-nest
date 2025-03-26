3ad50ac1465d0c2eb0cc678f41b4955d2b621789
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-26 20:47:40 -0300

    feat : Global Validation Pipe 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 속성을 요청에서 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 포함된 경우, 요청을 거부
+      transformOptions: {
+        enableImplicitConversion: true, // 타입 변환을 자동으로 수행
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
