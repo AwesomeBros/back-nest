773f7f3c9842a84ac2a875bc313b541c44669b51
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-26 20:44:56 -0300

    feat : 환경변수 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, ConfigModule.forRoot()],
   providers: [],
 })
 export class AppModule {}
