bb91840aa008e87c9908d1fedfe0005f2e1fa560
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-26 21:05:10 -0300

    feat : 로거 미들웨어 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
+import { RequestMiddleware } from './common/utils/logger.middleware';
 
 @Module({
   imports: [CommonModule, ConfigModule.forRoot()],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(RequestMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
